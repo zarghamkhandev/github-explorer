7dcd4b27ada94790cd936b06abc402f4051f1fb8
Zargham Khan <zarghamkhan980@gmail.com>
2022-03-20 17:59:14 +0100

    added vcard details for details component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { TuiSvgService } from '@taiga-ui/core';
 import { CIRCLE_FILL_ICON } from './tokens/circle-icon.token';
+import { TWITTER_ICON } from './tokens/twitter-icon.token';
 
 @Component({
   selector: 'app-root',
@@ -11,8 +12,9 @@ export class AppComponent {
   title = 'github-explorer';
   constructor(
     @Inject(TuiSvgService) tuiSvgService: TuiSvgService,
-    @Inject(CIRCLE_FILL_ICON) circleFillIcon: string
+    @Inject(CIRCLE_FILL_ICON) circleFillIcon: string,
+    @Inject(TWITTER_ICON) twitterIcon: string
   ) {
-    tuiSvgService.define({ circleFill: circleFillIcon });
+    tuiSvgService.define({ circleFill: circleFillIcon, twitter: twitterIcon });
   }
 }
--- src/app/pages/contributors/components/contributor-details/contributor-details.module.ts
+++ src/app/pages/contributors/components/contributor-details/contributor-details.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContributorDetailsComponent } from './contributor-details.component';
 import { TuiAvatarModule, TuiIslandModule } from '@taiga-ui/kit';
-import { TuiButtonModule } from '@taiga-ui/core';
+import { TuiButtonModule, TuiSvgModule } from '@taiga-ui/core';
 
 @NgModule({
   declarations: [ContributorDetailsComponent],
-  imports: [CommonModule, TuiIslandModule,TuiAvatarModule,TuiButtonModule],
+  imports: [
+    CommonModule,
+    TuiIslandModule,
+    TuiAvatarModule,
+    TuiButtonModule,
+    TuiSvgModule,
+  ],
   exports: [ContributorDetailsComponent],
 })
 export class ContributorDetailsModule {}
